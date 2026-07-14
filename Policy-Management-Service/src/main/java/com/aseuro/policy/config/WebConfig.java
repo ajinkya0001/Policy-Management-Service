a08ada1aa9d2d00ef11cba9cae549f940b87b323
Ajinkya Jadkar <ajinkya.jadkar@aseuro.in>
2026-07-14 17:17:55 +0530

    Complete Docker Compose setup and containerize full-stack application

--- Policy-Management-Service/src/main/java/com/aseuro/policy/config/WebConfig.java
+++ Policy-Management-Service/src/main/java/com/aseuro/policy/config/WebConfig.java
@@ -16,10 +16,18 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
+                // registry.addMapping("/**")
+                //         .allowedOrigins("http://localhost:5173")
+                //         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                //         .allowedHeaders("*");
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                .allowedOrigins(
+                        "http://localhost",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*");
 
             }
         };
